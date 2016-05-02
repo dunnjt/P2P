fc5aea9a88862eded3728c434a3c827fa431da38
dunnjt <johndunn@Johns-MacBook-Air-2.local>
2016-05-02 15:51:39 -0400

    TCP tester
    test

--- src/pkg490final/TCP/Main.java
+++ src/pkg490final/TCP/Main.java
@@ -16,13 +16,16 @@ public class Main {
     P2PHost host = null;
     try {
       // Start server
+      P2PClient.setFileLocation("c:/p2p");
+      P2PHost.setFileLocation("/Users/johndunn/Desktop/p2p");
       client = new P2PClient("Server", 33000, "test2.txt");
       client.start();
       client.sleep(1000);
 
       // Create client
       byte[] targetAdddress = {127, 0, 0, 1};
-      host = new P2PHost("CLIENT1", 33000, "test2.txt");
+      //(String name, int port, String localAddr, int localPort, String fileName)
+      host = new P2PHost("CLIENT1", 33000, "192.168.1.4", 33000, "test2.txt");
       //P2PClient client2 = new TCPClient("CLIENT2", 49000);
       host.start();
       host.sleep(1000);
